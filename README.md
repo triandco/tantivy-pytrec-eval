be195f4219bc5a36f6dc31f8af6607712d0dc578
Tri <tri@triandco.com>
2024-07-24 08:14:05 +0000

    feat: add elastic search retrieval

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Introduction
-This project evaluates [Tantivy](https://github.com/quickwit-oss/tantivy) and [Apache Lucene](https://github.com/apache/lucene) retrieval quality using standard ndcg@10 metric.
+This project evaluates [Tantivy](https://github.com/quickwit-oss/tantivy), [Elastic Search](https://github.com/elastic/elasticsearch) and [Apache Lucene](https://github.com/apache/lucene) retrieval quality using standard ndcg@10 metric.
 
 This project is based on [Beir](https://github.com/beir-cellar/beir). Index and retrieval task is performed by Tantivy and Lucene in their respective Tust and Java environment. 
 
-Search task is perform on a single field document which concat the title and the content of each document. For both Tantivy and Lucene, we use the default Tokeniser and BM25 scoring with no further tinkering. We use the default QueryParser for both Lucene and Tantivy which require query sanitisation. Our sanitisation method was the removal of special character for each implementation.
+For all candidate, we use the default settings for Tokenisation and BM25 settings. We use the default QueryParser for both Lucene and Tantivy which require query sanitisation since queries in multiple dataset contains special characters. Our sanitisation method was the removal of special character for each implementation. Meanwhile, for ElasticSearch, we use non-sanitised query string with a "type='best_field'" settings.
 
 Retrieval results are exported as tsv file which are then scored with [pytrec_eval](https://github.com/cvangysel/pytrec_eval). This approach allows us to manually examine search output and ensure both Tantivy's and Lucene's performance is scored by the same code base.
 
 Evaluation datasets are available on [Beir github](https://github.com/beir-cellar/beir).
 
 # Results
-| Dataset | Tantivy ndcg@10 | Apache Lucene ndcg@10 | [Beir BM25 Flat ndcg@10]((https://eval.ai/web/challenges/challenge-page/1897/leaderboard/4475)) |
+| Dataset | Tantivy ndcg@10 | Apache Lucene ndcg@10 | [Beir BM25 Flat ndcg@10]((https://eval.ai/web/challenges/challenge-page/1897/leaderboard/4475)) | Elastic Search |
 | - | - | - | - |
-| Scifact | 0.6251573122952132 | 0.632431156289918 | 0.679 |
-| NFCorpus | 0.20505084876906404 | 0.20712280950112716 | 0.322 |
-| TREC-COVID | 0.0362915780899568 | 0.035369826134136535 | 0.595 |
-| NQ | 0.2637953053727399 | 0.2803606345656689 | 0.306 |
+| Scifact | 0.6251573122952132 | 0.632431156289918 | 0.679 | 0.6563018879997284 |
+| NFCorpus | 0.20505084876906404 | 0.20712280950112716 | 0.322 | 0.2116375800036891 |
+| TREC-COVID | 0.0362915780899568 | 0.035369826134136535 | 0.595 | 0.05433894833185797 |
+| NQ | 0.2637953053727399 | 0.2803606345656689 | 0.306 | 0.310128528137924 |
 
 # Running evaluation
 ## 1. Prerequiste
@@ -37,14 +37,19 @@ data
 ```
 1. Run the following step to generate result for tantivy retrieval task. For instance, we are running retrieval for scifact corpus
 ```sh
-cd retrieval
+cd tantivy-retrieval
 cargo update
 cargo run -- scifact
 ```
-1. If ran successfully, a new file called ```result.tsv``` will be created in the dataset folder
+1. If ran successfully, a new file called ```result_tantivy.tsv``` will be created in the dataset folder
 
 ## 3. Runing lucene retrieval task
-1. 
+1. Run the following step to geenrate result for lucene retrieval task. For instance, we are running retrieval for scifact corpus
+```sh
+cd lucene-retrieval
+./gralew run --args="scifact"
+```
+2. Result of the run will be added to the dataset folder with the name ``result_lucene.tsv``
 
 ## 4. Running evaluation
 1. Run the following step to create virtualenv for python and install the necessary packages
@@ -54,7 +59,7 @@ python3 -m .venv
 source .venv/bin/activate
 pip install -r requirement.txt
 ```
-1. Run the evaluation script. For instance, we are running it for the scifact corpus
+1. Run the evaluation script. For instance, we are evaluate tantivy performance on the scifact corpus
 ```sh
-python main.py scifact
+python main.py scifact tantivy
 ```
---
+++ elasticsearch-retrieval/main.py
@@ -0,0 +1,145 @@
+from ssl import create_default_context
+from elasticsearch import Elasticsearch as ElasticSearch
+from elasticsearch.helpers import bulk
+
+import json
+import csv
+import os
+import sys
+
+class CorpusItem:
+    id: str
+    text: str
+    title: str
+
+    def __init__(self, _id, _text, _title): 
+        self.id = _id
+        self.text = _text 
+        self.title = _title
+
+    def __str__(self) -> str:
+        return f"CorpusItem({self.id}\t{self.title}\t{self.text})"
+
+class DatasetQuery:
+    id: str
+    text: str
+
+    def __init__(self, _id:str, _text:str):
+        self.id = _id
+        self.text = _text
+
+    def __str__(self) -> str:
+        return f"Query({self.id}: {self.text})"
+
+class QueryResult: 
+    qid: str
+    doc_id: str
+    score: float
+
+    def __init__(self, _qid:str, _doc_id:str, _score:float):
+        self.qid = _qid
+        self.doc_id = _doc_id
+        self.score = _score
+
+    def __str__(self) -> str:
+        return f"QueryResult({self.qid}\t{self.doc_id}\t{self.score})"
+
+
+def main():
+
+    dataset = sys.argv[1]
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    data_path = script_dir+"/../data/"+dataset
+
+    elastic_search = ElasticSearch(
+        ['https://localhost:9200'],
+        basic_auth=('elastic', 'TpOVaMXdOntY7LNIoRUz'),
+        ssl_context=create_default_context(cafile=script_dir+"/elasticsearch/config/certs/http_ca.crt")
+    )
+
+    corpus = load_corpus(data_path+"/corpus.jsonl")
+    print(f"Corpus loaded with {len(corpus)} item(s)")
+
+    queries = load_queries(data_path+"/queries.jsonl")
+    print(f"Queries loaded with {len(queries)} item(s)")
+
+    index(elastic_search, dataset, corpus)
+
+    results = search(elastic_search, dataset, queries)
+
+    print(f"A total of {len(results)} result(s) collected")
+    with open(data_path+"/result_elasticsearch.tsv", mode='w') as file:
+        tsv_writer = csv.writer(file, delimiter='\t')
+        for result in results:
+                tsv_writer.writerow((result.qid, result.doc_id, result.score))
+
+def load_queries(path) -> list[DatasetQuery]:
+    queries = []
+    with open(path) as file:
+        for line in file:
+            json_obj = json.loads(line)
+            queries.append(DatasetQuery(json_obj.get("_id"), json_obj.get("text")))
+
+    return queries
+
+def load_corpus(path) -> list[CorpusItem]:
+    corpus = []
+    with open(path) as file:
+        for line in file:
+            json_obj = json.loads(line)
+            corpus.append(CorpusItem(json_obj.get("_id"), json_obj.get("title"), json_obj.get("text")))
+    return corpus
+
+def search(es: ElasticSearch, index_name: str, queries: list[DatasetQuery]) -> list[QueryResult]:
+    output = []
+    for query in queries: 
+        req_body = {"query" : {"multi_match": {
+                "query": query.text, 
+                "type": "best_fields",
+                "fields": ["text", "title"],
+                "tie_breaker": 0.5
+                }},
+                "size": 10}
+        
+        results = es.search(
+            index = index_name, 
+            body = req_body
+        )
+        for hit in results["hits"]["hits"]:
+            output.append(QueryResult(query.id, hit["_id"], hit['_score']))
+    
+    return output
+        
+def split_array(arr: list, n:int) -> list[list]:
+    k, m = divmod(len(arr), n)
+    return [arr[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)]
+    
+def index(es: ElasticSearch, index_name: str, corpus: list[CorpusItem]):
+    if es.indices.exists(index=index_name):
+        es.indices.delete(index=index_name)
+
+    mapping = {
+        "mappings" : {
+            "properties" : {
+                "title": {"type": "text"},
+                "text": {"type": "text"}
+            }}}
+    
+    es.indices.create(index=index_name, body=mapping)
+
+    operations = []
+    for doc in corpus: 
+        operations.append({
+            '_index': index_name,
+            '_op_type': 'create',
+            "_id": doc.id,
+            "_source": {
+                "text": doc.text, 
+                "title": doc.title
+            }
+        })
+
+    bulk(es, operations, refresh="true")
+        
+if __name__ == '__main__':
+    main()
